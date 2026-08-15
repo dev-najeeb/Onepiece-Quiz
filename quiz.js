ca02840f3742be168cd45926d2c0150d2b03765b
najeeb dev <najeebkhoso057@gmail.com>
2026-08-15 22:54:18 +0500

    - Logic built for Selecting difficulties

--- quiz.js
+++ quiz.js
@@ -7,9 +7,17 @@ const quizbtn = document.getElementById("quizBtn");
 const options = document.querySelectorAll(".options");
 const quizContainer = document.querySelector(".quizContainer");
 const nextBtn = document.querySelector("#next");
+const easyBtn = document.querySelector("#easy");
+const mediumBtn = document.querySelector("#medium");
+const hardBtn = document.querySelector("#hard");
+const p = document.querySelector("#pHeading");
+const diffContainer = document.querySelector(".difficulties");
+
 currentIndex = 0;
-let lastIndex = 6;
+difficultyIndex = 0;
+let lastIndex = 10;
 let quizData;
+let questionData;
 let currQuestion;
 async function loadQuizData() {
   const response = await fetch("./assets/quizData.json");
@@ -17,44 +25,110 @@ async function loadQuizData() {
   console.log(quizData);
 }
 loadQuizData();
-function displayData() {
-  currQuestion = quizData[currentIndex];
-  const option = currQuestion.options;
+function displayData1() {
+  currData = quizData;
+  questionData = currData.difficulties.easy;
+  currQuestion = questionData[currentIndex];
   Question.innerText = currQuestion.question;
-  option1.innerText = option[0];
-  option2.innerText = option[1];
-  option3.innerText = option[2];
-  option4.innerText = option[3];
-  console.log(quizData);
+  const options = currQuestion.options;
+  option1.innerText = options[0];
+  option2.innerText = options[1];
+  option3.innerText = options[2];
+  option4.innerText = options[3];
   currentIndex++;
-  if (currentIndex === lastIndex) {
-    console.log("this was the last question");
-    nextBtn.classList.add("noClick");
-  };
 }
-quizbtn.addEventListener("click", () => {
-  console.log("function ran ");
+easyBtn.addEventListener("click", () => {
+  difficultyIndex = 1;
+  displayData1();
+  diffContainer.style.display = "none";
+  hardBtn.classList.add("hidden");
+  mediumBtn.classList.add("hidden");
+  easyBtn.classList.add("hidden");
+  p.classList.add("hidden");
+  options.forEach((btn) => {
+    btn.classList.add("visible");
+  });
   nextBtn.classList.add("active-button");
-  quizbtn.classList.add(".hidden");
-  options.forEach((btn)=>{
-  btn.classList.add("visible");
-  })
-  displayData();
 });
-nextBtn.addEventListener("click", () => {
-  displayData();
-  quizContainer.classList.remove("bgChange", "wrongAnws");
-  console.log("function ran");
+function displayData2() {
+  currData = quizData;
+  questionData = currData.difficulties.medium;
+  currQuestion = questionData[currentIndex];
+  Question.innerText = currQuestion.question;
+  const options = currQuestion.options;
+  option1.innerText = options[0];
+  option2.innerText = options[1];
+  option3.innerText = options[2];
+  option4.innerText = options[3];
+  currentIndex++;
+}
+mediumBtn.addEventListener("click", () => {
+  difficultyIndex = 2;
+  displayData2();
+  diffContainer.style.display = "none";
+  hardBtn.classList.add("hidden");
+  mediumBtn.classList.add("hidden");
+  easyBtn.classList.add("hidden");
+  p.classList.add("hidden");
+  options.forEach((btn) => {
+    btn.classList.add("visible");
+  });
+  nextBtn.classList.add("active-button");
 });
+function displayData3() {
+  currData = quizData;
+  questionData = currData.difficulties.hard;
+  currQuestion = questionData[currentIndex];
+  Question.innerText = currQuestion.question;
+  const options = currQuestion.options;
+  option1.innerText = options[0];
+  option2.innerText = options[1];
+  option3.innerText = options[2];
+  option4.innerText = options[3];
+  currentIndex++;
+}
+hardBtn.addEventListener("click", () => {
+  difficultyIndex = 3;
+  displayData3();
+  diffContainer.style.display = "none";
+  hardBtn.classList.add("hidden");
+  mediumBtn.classList.add("hidden");
+  easyBtn.classList.add("hidden");
+  p.classList.add("hidden");
+  nextBtn.classList.add("active-button")
+  options.forEach((btn) => {
+    btn.classList.add("visible");
+  });
+});
+
 options.forEach((SelcOption, index) => {
-  SelcOption.addEventListener("click", () => {
+  SelcOption.addEventListener("click", (event) => {
+    const clickedBtn = event.target;
     const correctAnswer = currQuestion.correct;
     if (correctAnswer === index) {
-      quizContainer.classList.add("bgChange");
+      clickedBtn.classList.add("correctAnws");
+      clickedBtn.classList.remove("wrongAnws");
       console.log("Correct answer!", correctAnswer);
     } else {
-      quizContainer.classList.add("wrongAnws");
+      clickedBtn.classList.add("wrongAnws");
       console.log("Incorrect");
     }
+  });
+});
+nextBtn.addEventListener("click", () => {
+  if (currentIndex === lastIndex) {
+    console.log("this was the last question");
+    return;
+  }
+  if (difficultyIndex === 1) {
+    displayData1();
+  } else if (difficultyIndex === 2) {
+    displayData2();
+  } else if (difficultyIndex === 3) {
+    displayData3();
+  }
+  console.log("function ran");
+  options.forEach((opt) => {
+    opt.classList.remove("correctAnws", "wrongAnws");
   });
 });
